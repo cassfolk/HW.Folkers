6ea960acae0e4fe990b847955997ecbbfb6a39d3
cassfolk <cassandra.folkers@gmail.com>
2020-09-25 03:29:06 -0700

    Updating in progress Python HW

--- 03-python-challenges/PyBank/main_all_but_net-change.py
+++ 03-python-challenges/PyBank/main_all_but_net-change.py
@@ -6,6 +6,7 @@ budget_csv = os.path.join("Resources", "budget_data.csv")
 total_months = 0
 total_money = 0
 net_change_values = []
+net_change_date = []
 
 with open(budget_csv) as csv_file:
     csv_budget = csv.reader(csv_file, delimiter=",")
@@ -17,7 +18,7 @@ with open(budget_csv) as csv_file:
     total_money += int(first_row[1])
 
     #*******THERE RIGHT THERE********
-    previous_next = int(first_row[1])
+    # previous_next = int(first_row[1])
 
     #count rows MINUS HEADER (above) for file
     for row in csv_budget:
@@ -30,15 +31,23 @@ with open(budget_csv) as csv_file:
 
         #get net change (aka diff between b2 & b3 etc)
         #*******THERE RIGHT THERE********
-        net_change = int(row[1]) - previous_next
-        
+        net_change = int(row[1]) - int(first_row[1])
         net_change_values += [net_change]
     
+        net_change_date += [row[0]]
+
         # print(previous_next)
-        print(int(row[1]))
+        # print(int(row[1]))
         # print(int(first_row[1]))
 
-#(f"{row[0]} ({row[1]})")
+increase = max(net_change_values)
+decrease = min(net_change_values)
+
+index_increase = net_change_values.index(increase)
+index_decrease = net_change_values.index(decrease)
+
+greatest_increase = f"{net_change_date[index_increase]} (${increase})"
+greatest_decrease = f"{net_change_date[index_decrease]} (${decrease})"
 
 average_change = sum(net_change_values) / len(net_change_values)
 
@@ -54,8 +63,8 @@ print(f"Total: ${total_money}")
 #reduce decimals down to 2 = .2f
 print(f"Average Change: ${average_change:.2f}") 
 
-#print(f"Greatest Increase in Profits: {greatest_increase}")    
-# print(f"Greatest Decrease in Profits: {great_decrease}")  
+print(f"Greatest Increase in Profits: {greatest_increase}")    
+print(f"Greatest Decrease in Profits: {greatest_decrease}")  
 
 
 #*******write code from scratch in a new document*********
