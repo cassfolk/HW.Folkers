1e25ddbf025888029e6082758eb1d5828fc8cff1
cassfolk <cassandra.folkers@gmail.com>
2020-11-06 04:39:36 -0800

    SQL hw

---
+++ 07-sql-challenge/EmployeeSQL/Queries.sql
@@ -0,0 +1,64 @@
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e
+JOIN salaries AS s ON e.emp_no = s.emp_no
+;
+
+-- 2.List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date between '01/01/1986' and '12/31/1986'
+;
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM departments AS d
+JOIN dept_manager AS dm 
+	ON (dm.dept_no = d.dept_no)
+		JOIN employees AS e
+			ON (e.emp_no = dm.emp_no)
+;
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e 
+JOIN dept_emp
+	ON (dept_emp.emp_no = e.emp_no)
+		JOIN departments AS d
+			ON (d.dept_no = dept_emp.dept_no)
+;
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT e.first_name, e.last_name, e.sex
+FROM employees AS e 
+WHERE e.first_name = 'Hercules'
+AND e.last_name LIKE 'B%'
+;
+
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e 
+JOIN dept_emp
+	ON (dept_emp.emp_no = e.emp_no)
+		JOIN departments AS d
+			ON (d.dept_no = dept_emp.dept_no)
+			WHERE dept_name = 'Sales'
+;
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e 
+JOIN dept_emp
+	ON (dept_emp.emp_no = e.emp_no)
+		JOIN departments AS d
+			ON (d.dept_no = dept_emp.dept_no)
+			WHERE dept_name IN ('Sales', 'Development')
+;
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, count(last_name) as "Frequency of Last Name"
+FROM employees
+GROUP BY last_name
+ORDER BY count(last_name) DESC
+;
---
+++ 07-sql-challenge/EmployeeSQL/table schemata.sql
@@ -0,0 +1,49 @@
+﻿---done import: titles, employees, departments, dept_manager, dept_emp, salaries
+
+SELECT * FROM salaries;
+
+CREATE TABLE titles (
+    title_id VARCHAR(20) NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    PRIMARY KEY (title_id)
+);
+
+CREATE TABLE employees (
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR(10) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    sex CHAR(2) NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE departments (
+    dept_no VARCHAR(20) NOT NULL,
+    dept_name VARCHAR(150) NOT NULL,
+    PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(20) NOT NULL,
+    emp_no INT NOT NULL,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(20) NOT NULL,
+    FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
