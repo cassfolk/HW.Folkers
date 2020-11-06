ff159c97cf2b3a745adb36c6bea1395493167096
cassfolk <cassandra.folkers@gmail.com>
2020-11-06 05:02:47 -0800

    Figured out "composity key" finally

--- 07-sql-challenge/EmployeeSQL/table schemata.sql
+++ 07-sql-challenge/EmployeeSQL/table schemata.sql
@@ -37,6 +37,7 @@ CREATE TABLE dept_manager (
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
     dept_no VARCHAR(20) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no)
     FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
 );
